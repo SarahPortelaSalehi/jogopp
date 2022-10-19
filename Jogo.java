9319633d3523928fa00229bc1bf7032e397b9d8e
Lidia Dias de Souza <99140148+lidia1805@users.noreply.github.com>
2022-10-18 22:19:02 -0300

    Update Jogo.java

--- Jogo.java
+++ Jogo.java
@@ -1,11 +1,37 @@
 package packge.busyness;
 
 
-public class Jogo extend Personagem {
+public class Jogo {
 	
-	public void ControlarAcoesDosPersonagens(String personagem) {
+	public void ControlarAcoesDosPersonagens(Personagem personagem) {
 		
-		
+		if (personagem instanceof Cavaleiro) {
+			Cavaleiro cavaleiro = (Cavaleiro)personagem;
+			cavaleiro.atacar();
+			cavaleiro.defender();
+			cavaleiro.saltar();
+			cavaleiro.andar();
+			cavaleiro.guardarItem();
+			cavaleiro.usarItem();
+			
+		}else if (personagem instanceof Mago) {
+			Mago mago = (Mago)personagem;
+			mago.invisibilidade();
+			mago.ultraRapidez();
+			mago.andar();
+			mago.guardarItem();
+			mago.usarItem();
+			
+		}else if (personagem instanceof Dragao) {
+			Dragao dragao = (Dragao)personagem;
+			dragao.atirarFogo();
+			dragao.voar();
+			dragao.morder();
+			dragao.andar();
+			dragao.guardarItem();
+			dragao.usarItem();
+			
+		}
 		
 		
 	}
