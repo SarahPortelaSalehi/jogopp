3b320c0c7d6d8b545cde34b9cdb48a43053d431e
Cibele Arahel Menezes Batista <99154448+cibelearahel@users.noreply.github.com>
2022-10-19 14:14:11 -0300

    updating dragon

--- Dragao.java
+++ Dragao.java
@@ -2,44 +2,52 @@ package packege.business;
 
 public class Dragao extends Personagem implements ComportamentoFeroz {
 	
+	
 	public Dragao(String nome) {
 		super(nome);
 	}
 
 	
 	public void atirarFogo() {
-		System.out.printf("Dragão [%s] atirou uma bola de fogo", getNome());
+		System.out.println();
+		System.out.printf("🐉 🔥 Dragão [%s] atirou uma bola de fogo", getNome());
 
 	}
 
 
 	public void voar() {
-		System.out.printf("Dragão [%s] está voando", getNome());
+		System.out.println();
+		System.out.printf("🐉 🔥 Dragão [%s] está voando", getNome());
 
 	}
 
 
 	public void morder() {
-		System.out.printf("Dragão [%s] mordeu seu alvo", getNome());
+		System.out.println();
+		System.out.printf("🐉 🔥 Dragão [%s] mordeu seu alvo", getNome());
 
 	}
 
 
 	public void andar() {
-		System.out.printf("Dragão [%s] está andando", getNome());
+		System.out.println();
+		System.out.printf("🐉 🔥 Dragão [%s] está andando", getNome());
 		
 	}
 
 
 	public void guardarItem() {
-		System.out.printf("Dragão [%s] guardou o amuleto de fogo", getNome());
+		System.out.println();
+		System.out.printf("🐉 🔥 Dragão [%s] guardou o amuleto de fogo", getNome());
 		
 	}
 
 	
 	public void usarItem() {
-		System.out.printf("Dragão [%s] usou o talismã dourado", getNome());
+		System.out.println();
+		System.out.printf("🐉 🔥 Dragão [%s] usou o talismã dourado", getNome());
 		
 	}
+
 
 }
