62aab8b1d360b1b88881b4f3a4b47cea8b9bf518
Lidia Dias de Souza <99140148+lidia1805@users.noreply.github.com>
2022-10-18 23:41:29 -0300

    Update Jogo.java

--- Jogo.java
+++ Jogo.java
@@ -7,26 +7,21 @@ public class Jogo {
 		
 		if (personagem instanceof Cavaleiro) {
 			Cavaleiro cavaleiro = (Cavaleiro)personagem;
-			cavaleiro.atacar();
-			cavaleiro.defender();
-			cavaleiro.saltar();
+			
 			cavaleiro.andar();
 			cavaleiro.guardarItem();
 			cavaleiro.usarItem();
 			
 		}else if (personagem instanceof Mago) {
 			Mago mago = (Mago)personagem;
-			mago.invisibilidade();
-			mago.ultraRapidez();
+			
 			mago.andar();
 			mago.guardarItem();
 			mago.usarItem();
 			
 		}else if (personagem instanceof Dragao) {
 			Dragao dragao = (Dragao)personagem;
-			dragao.atirarFogo();
-			dragao.voar();
-			dragao.morder();
+			
 			dragao.andar();
 			dragao.guardarItem();
 			dragao.usarItem();
