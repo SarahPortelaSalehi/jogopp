216664aa8f22b5dc309fe15225fe756c52de9aab
Cibele Arahel Menezes Batista <99154448+cibelearahel@users.noreply.github.com>
2022-10-19 14:15:38 -0300

    updating knight

--- Cavaleiro.java
+++ Cavaleiro.java
@@ -2,47 +2,52 @@ package packege.business;
 
 public class Cavaleiro extends Personagem implements ComportamentoHeroico {
 	
-	
 	public Cavaleiro(String nome) {	
 		super(nome);
 	}
 
 	
 	public void atacar() {
-		System.out.printf("Cavaleiro [%s] está atacando o Inimigo", getNome());
-
+		System.out.println();
+		System.out.printf("⚔︎ 🛡 Cavaleiro [%s] está atacando o Inimigo", getNome());
+		
 	}
-
 	
 	public void defender() {
-		System.out.printf("Cavaleiro [%s] está se defendendo contra um ataque", getNome());
+		System.out.println();
+		System.out.printf("⚔︎ 🛡 Cavaleiro [%s] está se defendendo contra um ataque", getNome());
 
 	}
 
 	
 	public void saltar() {
-		System.out.printf("Cavaleiro [%s] saltou", getNome());
+		System.out.println();
+		System.out.printf("⚔︎ 🛡 Cavaleiro [%s] saltou", getNome());
 
 	}
 
 
 	
 	public void andar() {
-		System.out.printf("Cavaleiro [%s] está andando", getNome());
+		System.out.println();
+		System.out.printf("⚔︎ 🛡 Cavaleiro [%s] está andando", getNome());
 		
 	}
 
 
 	
 	public void guardarItem() {
-		System.out.printf("Cavaleiro [%s] está guardando a espada Sagrada", getNome());
+		System.out.println();
+		System.out.printf("⚔︎ 🛡 Cavaleiro [%s] está guardando a espada Sagrada", getNome());
 		
 	}
 
 
 	
 	public void usarItem() {
-		System.out.printf("Cavaleiro [%s] usou a poção revitalizadora", getNome());
+		System.out.println();
+		System.out.printf("⚔︎ 🛡 Cavaleiro [%s] usou a poção revitalizadora", getNome());
+		
 		
 	}
 
